b40c35f4eb26292da45fcbbb0ab8533f055254ae
emrecanergor <emrecan.ergor@gmail.com>
2018-12-16 14:33:01 +0300

    Added Connection Lost Screen and Refresh Button

--- app/src/main/java/com/example/emre/dovizkuru/MainActivity.java
+++ app/src/main/java/com/example/emre/dovizkuru/MainActivity.java
@@ -7,10 +7,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -36,6 +38,8 @@ public class MainActivity extends ListActivity {
     private List<String> buyingList;
     private List<String> sellingList;
     private List<String> KodList;
+    private ConstraintLayout layout2;
+    private Button refreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +47,8 @@ public class MainActivity extends ListActivity {
         setContentView(R.layout.activity_main);
 
         lv = findViewById(android.R.id.list);
+        layout2 = findViewById(R.id.Layout2);
+        layout2.setVisibility(View.INVISIBLE);
 
         nationList = new ArrayList<String>();
         unitList = new ArrayList<String>();
@@ -50,104 +56,148 @@ public class MainActivity extends ListActivity {
         sellingList = new ArrayList<String>();
         KodList = new ArrayList<String>();
 
+        refreshButton = findViewById(R.id.buttonRefresh);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSetData();
+            }
+        });
 
-            Thread thread = new Thread(new Runnable() {
 
-                @Override
-                public void run() {
+        getSetData();
 
-                    try{
+    }
 
-                        URL url = new URL("http://tcmb.gov.tr/kurlar/today.xml");
-                        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-                        factory.setNamespaceAware(false);
-                        XmlPullParser parser = factory.newPullParser();
-                        parser.setInput(url.openConnection().getInputStream(), "UTF_8");
-                        boolean insideItem = false;
 
-                        int eventType = parser.getEventType();
-                        while(eventType != XmlPullParser.END_DOCUMENT) {
-                            if (eventType == XmlPullParser.START_TAG) {
-                                if (parser.getName().equalsIgnoreCase("Currency")) {
-                                    KodList.add(
-                                            parser.getAttributeValue(null, "Kod"));
-                                    insideItem = true;
-                                } else if (parser.getName().equalsIgnoreCase("Unit")) {
-                                    if (insideItem)
-                                        unitList.add(parser.nextText());
-                                } else if (parser.getName().equalsIgnoreCase("CurrencyName")) {
-                                    if (insideItem)
-                                        nationList.add(parser.nextText());
-                                } else if (parser.getName().equalsIgnoreCase("ForexBuying")) {
-                                    if (insideItem)
-                                        buyingList.add(parser.nextText());
-                                } else if (parser.getName().equalsIgnoreCase("ForexSelling")) {
-                                    if (insideItem)
-                                        sellingList.add(parser.nextText());
-                                }
-                            } else if (eventType == XmlPullParser.END_TAG &&
-                                    parser.getName().equalsIgnoreCase("Currency")) {
-                                insideItem = false;
+
+    private void getData()
+    {
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                try{
+
+                    KodList.clear();
+                    nationList.clear();
+                    unitList.clear();
+                    buyingList.clear();
+                    sellingList.clear();
+
+
+                    URL url = new URL("http://tcmb.gov.tr/kurlar/today.xml");
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    factory.setNamespaceAware(false);
+                    XmlPullParser parser = factory.newPullParser();
+                    parser.setInput(url.openConnection().getInputStream(), "UTF_8");
+                    boolean insideItem = false;
+
+                    int eventType = parser.getEventType();
+                    while(eventType != XmlPullParser.END_DOCUMENT) {
+                        if (eventType == XmlPullParser.START_TAG) {
+                            if (parser.getName().equalsIgnoreCase("Currency")) {
+                                KodList.add(
+                                        parser.getAttributeValue(null, "Kod"));
+                                insideItem = true;
+                            } else if (parser.getName().equalsIgnoreCase("Unit")) {
+                                if (insideItem)
+                                    unitList.add(parser.nextText());
+                            } else if (parser.getName().equalsIgnoreCase("CurrencyName")) {
+                                if (insideItem)
+                                    nationList.add(parser.nextText());
+                            } else if (parser.getName().equalsIgnoreCase("ForexBuying")) {
+                                if (insideItem)
+                                    buyingList.add(parser.nextText());
+                            } else if (parser.getName().equalsIgnoreCase("ForexSelling")) {
+                                if (insideItem)
+                                    sellingList.add(parser.nextText());
                             }
-                            eventType = parser.next();
+                        } else if (eventType == XmlPullParser.END_TAG &&
+                                parser.getName().equalsIgnoreCase("Currency")) {
+                            insideItem = false;
                         }
+                        eventType = parser.next();
+                    }
 
+                }
+                catch(MalformedURLException ex) {
+                    ex.printStackTrace();
+                }
+                catch(XmlPullParserException ex){
+                    ex.printStackTrace();
+                }
+                catch(IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+
+    private void setData()
+    {
+
+        try {
+            //for correct thread handling
+            Handler refresh = new Handler(Looper.getMainLooper());
+            refresh.post(new Runnable() {
+                public void run()
+                {
+                    List<String> tempList = new ArrayList<String>();
+
+                    //List size control.
+                    if(KodList.size() == 0)
+                    {
+                        layout2.setVisibility(View.VISIBLE);
+                        return;
                     }
-                    catch(MalformedURLException ex) {
-                        ex.printStackTrace();
+                    else
+                    {
+                        layout2.setVisibility(View.INVISIBLE);
                     }
-                    catch(XmlPullParserException ex){
-                        ex.printStackTrace();
+
+
+                    for(int i=0; i<KodList.size();i++) {
+                        tempList.add(KodList.get(i)+
+                                "\t\t  ..-..  "+unitList.get(i)+
+                                "\t\t  ..-..  "+buyingList.get(i)+
+                                "\t\t  ..-..  "+sellingList.get(i));
                     }
-                    catch(IOException ex){
-                        ex.printStackTrace();
-                    }
+                    ArrayAdapter adapter = new
+                            ArrayAdapter(context, android.R.layout.simple_list_item_1, tempList);
+                    //setListAdapter(adapter);
+                    lv.setAdapter(adapter);
                 }
             });
 
-        thread.start();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
 
 
-
+    private void getSetData()
+    {
+        getData();
 
         Timer timer = new Timer();
         TimerTask t = new TimerTask() {
             @Override
             public void run() {
-
-                try {
-                    //for correct thread handling
-                    Handler refresh = new Handler(Looper.getMainLooper());
-                    refresh.post(new Runnable() {
-                        public void run()
-                        {
-                            List<String> tempList = new ArrayList<String>();
-
-                            for(int i=0; i<KodList.size();i++) {
-                                tempList.add(KodList.get(i)+
-                                        "\t\t  ..-..  "+unitList.get(i)+
-                                        "\t\t  ..-..  "+buyingList.get(i)+
-                                        "\t\t  ..-..  "+sellingList.get(i));
-                        }
-                            ArrayAdapter adapter = new
-                                    ArrayAdapter(context, android.R.layout.simple_list_item_1, tempList);
-                            //setListAdapter(adapter);
-                            lv.setAdapter(adapter);
-                        }
-                    });
-
-            }
-                catch(Exception ex) {
-                    ex.printStackTrace();
-                }
-
+                setData();
             }
         };
         //delay of data receiving
-        timer.schedule(t,400);
-
-
+        timer.schedule(t,700);
     }
+
+
 
 
 }
